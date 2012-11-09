c4e0d9aa93f7484317e4429355783bbf664589d8
Franklin Chen <franklinchen@franklinchen.com>
2012-11-08 23:17:48 -0500

    Add more tests, including round trip properties. Finish implementation.

--- Time1.hs
+++ Time1.hs
@@ -3,18 +3,112 @@
 -}
 module Time1 where
 
-{-|
-  Convert time string to a time in seconds.
--}
-timeToSeconds :: String    -- ^ should be in format ...HHHH:MM:SS
-              -> Maybe Int -- ^ time in seconds, if successful parse
-timeToSeconds = undefined
--- TODO
+newtype Hours = Hours Int
+
+newHours h | h >= 0    = Just $ Hours h
+           | otherwise = Nothing
+
+newtype Minutes = Minutes Int
+
+newMinutes m | m >= 0 && m < 60 = Just $ Minutes m
+             | otherwise        = Nothing
+
+newtype Seconds = Seconds Int
+
+newSeconds s | s >= 0 && s < 60 = Just $ Seconds s
+             | otherwise        = Nothing
 
 {-|
   Convert time in seconds to string format
 -}
-secondsToTime :: Int    -- ^ time in seconds >= 0
+secondsToTime :: Int    -- ^ @totalSeconds@ >= 0
               -> String -- ^ ...HHHH:MM:SS
-secondsToTime = undefined
--- TODO
+secondsToTime totalSeconds =
+  formatHours(hours) ++
+    ":" ++ formatMinutes(minutes) ++
+    ":" ++ formatSeconds(seconds)
+  where (hours, minutes, seconds) = secondsToClock totalSeconds
+
+{-|
+  Precondition @totalSeconds@ >= 0
+
+  Postconditions: @hours@ >= 0, 0 <= @minutes@ < 60, 0 <= @seconds@ < 60
+-}
+secondsToClock :: Int -> (Hours, Minutes, Seconds)
+secondsToClock totalSeconds =
+  (Hours hours, Minutes minutes, Seconds seconds)
+  where hours = totalSeconds `div` (60 * 60)
+        remainingSeconds = totalSeconds - hours * (60 * 60)
+        minutes = remainingSeconds `div` 60
+        seconds = remainingSeconds - minutes * 60
+
+formatHours :: Hours -> String
+formatHours (Hours h) = show h
+
+{-|
+  Leading 0 if necessary
+-}
+formatMinutes :: Minutes -> String
+formatMinutes (Minutes m)
+  | m < 10    = '0' : show m
+  | otherwise = show m
+
+{-|
+  Leading 0 if necessary
+-}
+formatSeconds :: Seconds -> String
+formatSeconds (Seconds s)
+  | s < 10    = '0' : show s
+  | otherwise = show s
+
+{-|
+  Convert time string to a time in seconds.
+
+  For illustration, do this by hand without regex, parsing, or monads!
+
+  Use two phases: parse colon-separated list of digits,
+  then verify the constraints on that list.
+
+  Make sure minutes and seconds are exactly two digits.
+-}
+timeToSeconds :: String    -- ^ should be in format ...HHHH:MM:SS
+              -> Maybe Int -- ^ time in seconds, if successful parse
+timeToSeconds s = 
+  case splitDigitsAtColon s of
+    Nothing -> Nothing
+    Just [hoursString,
+         minutesString @ [_, _],
+         secondsString @ [_, _]] ->
+      let
+        hours = read hoursString
+        minutes = read minutesString
+        seconds = read secondsString
+      in
+        if minutes >= 60 || seconds >= 60 then
+          Nothing
+        else
+          Just $ hours * 60 * 60 + minutes * 60 + seconds
+    _ -> Nothing
+
+{-|
+  Helper to split a string at colons on digits to get numerical strings.
+
+  This should be in a standard library.
+-}
+splitDigitsAtColon :: String -> Maybe [String]
+splitDigitsAtColon xs = splitDigitsAtColon' xs ""
+
+{-|
+  TODO: Could write monadically, but not now.
+-}
+splitDigitsAtColon' :: String
+                -> String         -- ^ stack for current digit string
+                -> Maybe [String] -- ^ string with only 0 through 9
+splitDigitsAtColon' [] stack = Just [reverse stack]
+splitDigitsAtColon' (':':xs) stack =
+  case splitDigitsAtColon' xs [] of
+    Nothing   -> Nothing
+    Just stacks -> Just $ (reverse stack):stacks
+splitDigitsAtColon' (x:xs) stack
+  | x >= '0' && x <= '9' = splitDigitsAtColon' xs $ x:stack
+splitDigitsAtColon' _ stack = Nothing
--- test.hs
+++ test.hs
@@ -12,11 +12,39 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Control.Monad
 
+import qualified Data.List as List
+
 import qualified Time1 as T
 
 main = $(defaultMainGenerator)
 
 -- Basic HUnit tests
+
+-- Utils
+case_splitDigitsAtColon_digits =
+  T.splitDigitsAtColon "12:345:678" @?= Just ["12", "345", "678"]
+
+case_splitDigitsAtColon_not_digits =
+  T.splitDigitsAtColon "12:345:6X8" @?= Nothing
+
+generateDigits = listOf1 $ elements ['0'..'9']
+
+{-|
+  Splitting and joining round trip
+-}
+prop_round_trip_splitDigitsAtColon_and_intercalate =
+  forAll (do
+            h <- generateDigits
+            m <- generateDigits
+            s <- generateDigits
+            return $ h ++
+                     ":" ++ m ++
+                     ":" ++ s
+         ) $
+  \xs ->
+  case T.splitDigitsAtColon xs of
+    Nothing        -> False
+    Just digitList -> List.intercalate ":" digitList == xs
 
 -- timeToSeconds success
 case_timeToSeconds_00_00_00 =
@@ -47,3 +75,52 @@ case_secondsToTime_0 =
 
 case_secondsToTime_45296 =
   T.secondsToTime 45296 @?= "12:34:56"
+
+-- formatMinutes
+case_formatMinutes_5 =
+  T.formatMinutes (T.Minutes 5) @?= "05"
+
+-- Some interesting properties to check
+
+{-|
+  Converting from seconds to a time string and back should always
+  succeed, and give back just the original seconds.
+
+  Limit the range of natural numbers for the test.
+-}
+prop_round_trip_seconds_to_seconds =
+  forAll (choose (0, 3000000)) $
+  \seconds ->
+  T.timeToSeconds (T.secondsToTime seconds) == Just seconds
+
+{-
+  Test hours just up to 2000
+-}
+instance Arbitrary T.Hours where
+  arbitrary = liftM T.Hours $ choose (0, 2000)
+
+instance Arbitrary T.Minutes where
+  arbitrary = liftM T.Minutes $ choose (0, 59)
+
+instance Arbitrary T.Seconds where
+  arbitrary = liftM T.Seconds $ choose (0, 59)
+
+{-|
+  Converting from a time string to seconds successfully should
+  convert back to the original time string.
+
+  Test with some legal time strings.
+-}
+prop_round_trip_time_to_time =
+  forAll (do
+            h <- arbitrary
+            m <- arbitrary
+            s <- arbitrary
+            return $ T.formatHours h ++
+                     ":" ++ T.formatMinutes m ++
+                     ":" ++ T.formatSeconds s
+         ) $
+  \timeString ->
+  case T.timeToSeconds timeString of
+    Nothing           -> False
+    Just totalSeconds -> T.secondsToTime totalSeconds == timeString
